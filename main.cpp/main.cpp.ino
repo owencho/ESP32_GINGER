df0beaa89c8e7cf5cabdc4b343266657af3e1d5f
owencho <owencho200@gmail.com>
2020-10-20 15:04:51 +0800

    now can control led

--- main.cpp/main.cpp.ino
+++ main.cpp/main.cpp.ino
@@ -9,7 +9,7 @@
 
 const char* ssid = "VB77";
 const char* password = "Bottas4LifeVB77";
-
+DynamicJsonDocument data1(512);
 WebServer server(80);
 //setRoom 
 void setLED(){
@@ -21,15 +21,13 @@ void setLED(){
         // if the file didn't open, print an error:
         Serial.print(F("Error parsing JSON "));
         Serial.println(error.c_str());
- 
+        
         String msg = error.c_str();
- 
         server.send(400, F("text/html"),
                 "Error in parsin json body! " + msg);
  
     } else {
         JsonObject postObj = doc.as<JsonObject>();
- 
         Serial.print(F("HTTP Method: "));
         Serial.println(server.method());
  
@@ -39,33 +37,45 @@ void setLED(){
                 Serial.println(F("done."));
  
                 // Here store data or doing operation
-                Serial.print(F("\n name :"));
-                Serial.printf(doc["name"]);
-                Serial.print(F("\n type: "));
+                if(!strcmp(doc["type"],"HIGH")){
+                  digitalWrite(2,HIGH);
+                }
+                else if (!strcmp(doc["type"],"LOW")){
+                  digitalWrite(2,LOW); 
+                }
+                else{
+                  DynamicJsonDocument doc(512);
+                  doc["status"] = "INVALID TYPE";
+                  String buf;
+                  serializeJson(doc, buf);
+                  server.send(400, F("application/json"), buf);
+                  Serial.print(F("\n done."));
+                  return;
+                }
+                data1["type"] = doc["type"]; 
+                
+                Serial.print(F("\n Type:"));
                 Serial.printf(doc["type"]);
-                Serial.print(F("\n"));
+            
                 // Create the response
                 // To get the status of the result you can get the http status so
                 // this part can be unusefully
                 DynamicJsonDocument doc(512);
                 doc["status"] = "OK";
- 
-                Serial.print(F("Stream... \n"));
                 String buf;
                 serializeJson(doc, buf);
- 
                 server.send(201, F("application/json"), buf);
-                Serial.print(F("done."));
+                Serial.print(F("\n done."));
  
-            }else {
+            }
+            else {
                 DynamicJsonDocument doc(512);
                 doc["status"] = "KO";
                 doc["message"] = F("No data found, or incorrect!");
- 
+                //send error message
                 Serial.print(F("Stream..."));
                 String buf;
                 serializeJson(doc, buf);
- 
                 server.send(400, F("application/json"), buf);
                 Serial.print(F("done."));
             }
@@ -73,6 +83,16 @@ void setLED(){
     }
 }
 //getSetting
+/*
+void getLED(){
+    StaticJsonDocument doc(512);
+    Serial.print(F("Stream..."));
+    String buf;
+    serializeJson(doc, buf);
+    server.send(200, F("application/json"), buf);
+    Serial.print(F("done."));
+}
+*/
 void getSettings(){
     DynamicJsonDocument doc(512);
     doc["ip"] = WiFi.localIP().toString();
@@ -112,60 +132,62 @@ void restServerRouting() {
     //edit here to have the sub root folder
     server.on(F("/helloWorld"), HTTP_GET, getHelloWorld);
     server.on(F("/settings"), HTTP_GET, getSettings);
+    //server.on(F("/getLED"), HTTP_GET, getLED);
     //POST
     server.on(F("/setLED"), HTTP_POST, setLED);
 }
 
 // Manage not found URL
 void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    for (uint8_t i = 0; i < server.args(); i++) {
+      message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    }
+    server.send(404, "text/plain", message);
 }
 
 void setup(void) {
-  //init usart
-  Serial.begin(115200);
-  //configure wifi
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-  
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  
-  // Activate mDNS this is used to be able to connect to the server
-  // with local DNS hostmane esp32.local
-  if (MDNS.begin("esp32")) {
-    Serial.println("MDNS responder started");
-  }
-  
-  // Set server routing
-  restServerRouting();
-  // Set not found response
-  server.onNotFound(handleNotFound);
-  // Start server
-  server.begin();
-  Serial.println("HTTP server started");
+    //init usart
+    Serial.begin(115200);
+    //configure wifi
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("");
+    
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    // Activate mDNS this is used to be able to connect to the server
+    // with local DNS hostmane esp32.local
+    if (MDNS.begin("esp32")) {
+      Serial.println("MDNS responder started");
+    }
+    //set pin 2 LED as output
+    pinMode(2,OUTPUT);
+    // Set server routing
+    restServerRouting();
+    // Set not found response
+    server.onNotFound(handleNotFound);
+    // Start server
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
 void loop(void) {
-  server.handleClient();
+    server.handleClient();
 }
